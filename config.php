25d63a07632d23924d83b52862986cfcfcf8fab4
Ivaylo Marinkov <git@tsarstva.bg>
2015-04-11 02:14:55 +0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+	// This file contains settings for the tsarstva.bg.url.shortener
+	$timezone = "Europe/Helsinki";
+	$db_host = "localhost";
+	$db_name = "<DATABASE NAME GOES HERE>";
+	$db_password = "<DATABASE PASSWORD GOES HERE>";
+?>
